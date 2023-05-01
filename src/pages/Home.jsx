9f73deef7846cd10cf1dd9e4518648c7811b29e2
Shuyibe48 <gthrough48@gmail.com>
2023-05-02 01:51:20 +0600

    recipe card section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Chefs from '../components/Chefs';
+import RecipeCard from '../components/RecipeCard';
 
 const Home = () => {
     return (
         <>
             <Banner />
             <Chefs />
+            <RecipeCard />
         </>
     );
 };
