0d37cfddc88a5e86d4ef95d79193180a0ab06ab5
Shuyibe48 <gthrough48@gmail.com>
2023-05-02 01:08:37 +0600

    Chef section added hard coded

---
+++ src/components/Chefs.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Chefs = () => {
+    const chefs = [
+        {
+            id: 1,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'John Doe',
+            experience: 10,
+            recipes: 50,
+            likes: 100,
+        },
+        {
+            id: 2,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'Jane Smith',
+            experience: 15,
+            recipes: 75,
+            likes: 150,
+        },
+        {
+            id: 3,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'Jane Smith',
+            experience: 15,
+            recipes: 75,
+            likes: 150,
+        },
+        {
+            id: 4,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'Jane Smith',
+            experience: 15,
+            recipes: 75,
+            likes: 150,
+        },
+        {
+            id: 5,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'Jane Smith',
+            experience: 15,
+            recipes: 75,
+            likes: 150,
+        },
+        {
+            id: 6,
+            image: 'https://images.unsplash.com/photo-1583394293214-28ded15ee548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            name: 'Jane Smith',
+            experience: 15,
+            recipes: 75,
+            likes: 150,
+        }
+    ];
+
+    return (
+        <div className="container px-4 lg:px-0 mx-auto py-8">
+            <h2 className="text-2xl font-bold mb-8">Top Chefs</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {chefs.map((chef) => (
+                    <div className="bg-white rounded-md shadow-md p-4" key={chef.id}>
+                        <img src={chef.image} alt={chef.name} className="w-full rounded-md mb-4" />
+                        <h3 className="text-xl font-medium">{chef.name}</h3>
+                        <p className="text-gray-500 mb-2">{chef.experience} years of experience</p>
+                        <p className="text-gray-500 mb-2">{chef.recipes} recipes</p>
+                        <p className="text-gray-500 mb-4">{chef.likes} likes</p>
+                        <button className="bg-gray-800 text-white py-2 px-4 rounded-full transition-all duration-200 hover:bg-gray-900">View Recipes</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Chefs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Chefs from '../components/Chefs';
 
 const Home = () => {
     return (
         <>
-            <Banner/>
+            <Banner />
+            <Chefs />
         </>
     );
 };
