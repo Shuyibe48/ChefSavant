be1cc8b9cea463c9465a66d30c005f3cb6964066
Shuyibe48 <gthrough48@gmail.com>
2023-05-02 00:27:53 +0600

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Menubar from './Menubar';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Home = () => {
     return (
         <>
             <Menubar />
             <Outlet />
+            <Footer />
         </>
     );
 };
