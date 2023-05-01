2ff650bb1cc65c3b08530884f8fcab9f1fa661dd
Shuyibe48 <gthrough48@gmail.com>
2023-05-02 00:20:22 +0600

    navbar added

--- src/pages/Menubar.jsx
+++ src/pages/Menubar.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Menubar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div>
-            <h1>menu bar</h1>
-        </div>
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        <span className="font-semibold text-xl tracking-tight">Recipe Website</span>
+                    </div>
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                Home
+                            </Link>
+                            <Link to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                Blog
+                            </Link>
+                            <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                Login
+                            </Link>
+                            <Link to="/register" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                Register
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="-mr-2 flex md:hidden">
+                        <button onClick={handleMenuClick} type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span className="sr-only">Open main menu</span>
+                            <FontAwesomeIcon icon={faBars} />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Home
+                    </Link>
+                    <Link to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Blog
+                    </Link>
+                    <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Login
+                    </Link>
+                    <Link to="/register" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Register
+                    </Link>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import Blog from "../pages/Blog";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <Home />
+        path: '/',
+        element: <Home />,
+        children: [
+            {
+                path: 'blog',
+                element: <Blog/>
+            },
+            {
+                path: 'login',
+                element: <Login/>
+            },
+            {
+                path: 'register',
+                element: <Register/>
+            }
+        ]
     },
 ]);
 
