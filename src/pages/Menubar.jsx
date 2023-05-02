135e2c84912ea2cf5288c052f02dfd8d07e2e0ed
Shuyibe48 <gthrough48@gmail.com>
2023-05-02 16:49:06 +0600

    menubar updated

--- src/pages/Menubar.jsx
+++ src/pages/Menubar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Menubar = () => {
+
+    const signOut = () => {
+        console.log('hi')
+    }
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -11,56 +13,58 @@ const Menubar = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    return (
-        <nav className="bg-gray-800">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                    <div className="flex items-center flex-shrink-0 text-white mr-6">
-                        <span className="font-semibold text-xl tracking-tight">ChefSavant</span>
-                    </div>
-                    <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
-                            <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Home
-                            </Link>
-                            <Link to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Blog
-                            </Link>
-                            <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Login
-                            </Link>
-                            <Link to="/register" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Register
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="-mr-2 flex md:hidden">
-                        <button onClick={handleMenuClick} type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                            <span className="sr-only">Open main menu</span>
-                            <FontAwesomeIcon icon={faBars} />
-                        </button>
-                    </div>
-                </div>
-            </div>
+    const user = { displayName: 'Fahim Ahmed', photoURL: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' }
 
-            <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <Link to="/">
+                    <span className="font-semibold text-xl tracking-tight">ChefSavant</span>
+                </Link>
+            </div>
+            <div className="block lg:hidden">
+                <button onClick={handleMenuClick} className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3a3 3 0 013-3h14a3 3 0 010 6H3a3 3 0 01-3-3zm0 8a3 3 0 013-3h14a3 3 0 010 6H3a3 3 0 01-3-3zm0 8a3 3 0 013-3h14a3 3 0 010 6H3a3 3 0 01-3-3z" /></svg>
+                </button>
+            </div>
+            <div className={`w-full ${isMenuOpen ? 'block' : 'hidden'} lg:block flex-grow lg:flex lg:items-center lg:w-auto`}>
+                <div className="text-sm lg:flex-grow">
+                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
                         Home
                     </Link>
-                    <Link to="/blog" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                    <Link to="/blog" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
                         Blog
                     </Link>
-                    <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                        Login
-                    </Link>
-                    <Link to="/register" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                        Register
-                    </Link>
+                </div>
+                <div className="flex items-center">
+                    {user ? (
+                        <>
+                            <div className="relative profile-picture">
+                                <img className="h-8 w-8 rounded-full cursor-pointer" src={user.photoURL || defaultProfilePicture} alt={user.displayName || 'Profile picture'} />
+                                {user.displayName && (
+                                    <div className="absolute profile-name top-0 left-full -ml-1 mt-2 px-2 py-1 bg-white rounded-lg shadow-lg">
+                                        {user.displayName}
+                                    </div>
+                                )}
+                            </div>
+                            <button className="ml-4 px-4 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white" onClick={signOut}>
+                                Log Out
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login" className="text-gray-200 hover:text-white mr-4">
+                                Log In
+                            </Link>
+                            <Link to="/register" className="px-4 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
-    );
-};
+    )
+}
 
 export default Menubar;
