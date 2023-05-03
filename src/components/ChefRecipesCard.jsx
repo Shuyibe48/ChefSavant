e8e4b89c8b35baf17f866f83ffcaa28c9230c7ee
Shuyibe48 <gthrough48@gmail.com>
2023-05-03 18:02:41 +0600

    toast install and added on ui

--- src/components/ChefRecipesCard.jsx
+++ src/components/ChefRecipesCard.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { StarIcon, HeartIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const RecipeCard = ({ chefRecipe }) => {
@@ -12,6 +14,8 @@ const RecipeCard = ({ chefRecipe }) => {
     const handleFavoriteClick = () => {
         setIsFavorite(!isFavorite);
     };
+
+    const notify = () => toast(`${recipeName} added on your favorite list!`);
 
     return (
         <div className="bg-white rounded-lg overflow-hidden shadow-md">
@@ -30,9 +34,12 @@ const RecipeCard = ({ chefRecipe }) => {
                     <StarIcon className="h-5 w-5 text-yellow-400 mr-1" />
                     <span className="font-semibold text-gray-600">{chefRatings.toFixed(1)}</span>
                 </div>
-                <button className={`mt-4 bg-gray-800 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline`} disabled={isFavorite === true} onClick={handleFavoriteClick}>
-                    {isFavorite ? <HeartIcon className="h-6 w-6 text-pink-500"/> : <HeartIcon className="h-6 w-6 text-white"/>}
-                </button>
+                <div onClick={notify}>
+                    <button className={`mt-4 bg-gray-800 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline`} disabled={isFavorite === true} onClick={handleFavoriteClick}>
+                        {isFavorite ? <HeartIcon className="h-6 w-6 text-pink-500" /> : <HeartIcon className="h-6 w-6 text-white" />}
+                    </button>
+                        <ToastContainer />
+                </div>
             </div>
         </div>
     );
